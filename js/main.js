41f828622018ad961fba59c5822fb3689ae8beaf
woat <ahmad@navlys.com>
2017-02-18 02:15:51 -0500

    scrollify animations lol

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
 $(function() {
 	$.scrollify({
 		section : ".hero",
-    interstitialSection:".hero, .end"
-	});
+    interstitialSection:".hero, .end",
+    before: function(i, panels) {
+      var ref = panels[i].attr("data-section-name");
+
+      if(ref==="speed") {
+        $(".sec2ColLeft").addClass("fadeInLeftBig");
+        $(".sec2ColRight").addClass("fadeInRightBig");
+      }
+      if(ref==="privacy") {
+        $(".sec3ColLeft").addClass("fadeInLeftBig");
+        $(".sec3ColRight").addClass("fadeInRightBig");
+      }
+      if(ref!=="speed") {
+        $(".sec2ColLeft").removeClass("fadeInLeftBig");
+        $(".sec2ColRight").removeClass("fadeInRightBig");
+      }
+      if(ref!=="privacy") {
+        $(".sec3ColLeft").removeClass("fadeInLeftBig");
+        $(".sec3ColRight").removeClass("fadeInRightBig");
+      }
+    }
+  });
 });
