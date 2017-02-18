8a29f4423a7a3f365dbfd098c25c28c06e041cc1
woat <ahmad@navlys.com>
2017-02-18 12:25:28 -0500

    changed ref to numbers

--- js/main.js
+++ js/main.js
@@ -4,27 +4,27 @@ $(function() {
     interstitialSection:".hero, .end",
     before: function(i, panels) {
       var ref = panels[i].attr("data-section-name");
-      if(ref==="home") {
-        $(".sec1").addClass("fadeIn")
+      if(i===0) {
+        $(".sec0").addClass("fadeIn")
       }
-      if(ref==="speed") {
+      if(i===1) {
+        $(".sec1ColLeft").addClass("fadeInLeftBig");
+        $(".sec1ColRight").addClass("fadeInRightBig");
+      }
+      if(i===2) {
         $(".sec2ColLeft").addClass("fadeInLeftBig");
         $(".sec2ColRight").addClass("fadeInRightBig");
       }
-      if(ref==="privacy") {
-        $(".sec3ColLeft").addClass("fadeInLeftBig");
-        $(".sec3ColRight").addClass("fadeInRightBig");
+      if(i!==0) {
+        $(".sec0").removeClass("fadeIn")
       }
-      if(ref!=="home") {
-        $(".sec1").removeClass("fadeIn")
+      if(i!==1) {
+        $(".sec1ColLeft").removeClass("fadeInLeftBig");
+        $(".sec1ColRight").removeClass("fadeInRightBig");
       }
-      if(ref!=="speed") {
+      if(i!==2) {
         $(".sec2ColLeft").removeClass("fadeInLeftBig");
         $(".sec2ColRight").removeClass("fadeInRightBig");
-      }
-      if(ref!=="privacy") {
-        $(".sec3ColLeft").removeClass("fadeInLeftBig");
-        $(".sec3ColRight").removeClass("fadeInRightBig");
       }
     }
   });
