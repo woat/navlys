539eac4f7c3ac7b30dda3e3d92967c310d6773ad
woat <ahmad@navlys.com>
2017-03-03 15:44:47 -0500

    mobi hotfix

--- js/main.js
+++ js/main.js
@@ -1,43 +1,49 @@
 $(function() {
-	$.scrollify({
-		section : ".hero",
-    interstitialSection:".hero, .end",
-    before: function(i, panels) {
-      var ref = panels[i].attr("data-section-name");
-      if(i===0) {
-        $(".sec0").addClass("fadeIn")
+  if (screen.width <= 540) {
+    $.scrollify.disable();
+    $(".title").addClass("has-text-centered");
+    $(".subtitle").addClass("has-text-centered");
+  } else {
+  	$.scrollify({
+  		section : ".hero",
+      interstitialSection:".hero, .end",
+      before: function(i, panels) {
+        var ref = panels[i].attr("data-section-name");
+        if(i===0) {
+          $(".sec0").addClass("fadeIn")
+        }
+        if(i===1) {
+          $(".sec1ColLeft").addClass("fadeInLeftBig");
+          $(".sec1ColRight").addClass("fadeInRightBig");
+        }
+        if(i==2) {
+          $(".box").addClass("zoomIn");
+          $(".img").addClass("fadeInDownBig");
+          $(".is-3").addClass("zoomIn");
+          $(".is-5").addClass("fadeInUpBig");
+        }
+        if(i===3) {
+          $(".sec3ColLeft").addClass("fadeInLeftBig");
+          $(".sec3ColRight").addClass("fadeInRightBig");
+        }
+        if(i!==0) {
+          $(".sec0").removeClass("fadeIn")
+        }
+        if(i!==1) {
+          $(".sec1ColLeft").removeClass("fadeInLeftBig");
+          $(".sec1ColRight").removeClass("fadeInRightBig");
+        }
+        if(i!==2) {
+          $(".box").removeClass("zoomIn");
+          $(".img").removeClass("fadeInDownBig");
+          $(".is-3").removeClass("zoomIn");
+          $(".is-5").removeClass("fadeInUpBig");
+        }
+        if(i!==3) {
+          $(".sec3ColLeft").removeClass("fadeInLeftBig");
+          $(".sec3ColRight").removeClass("fadeInRightBig");
+        }
       }
-      if(i===1) {
-        $(".sec1ColLeft").addClass("fadeInLeftBig");
-        $(".sec1ColRight").addClass("fadeInRightBig");
-      }
-      if(i==2) {
-        $(".box").addClass("zoomIn");
-        $(".img").addClass("fadeInDownBig");
-        $(".is-3").addClass("zoomIn");
-        $(".is-5").addClass("fadeInUpBig");
-      }
-      if(i===3) {
-        $(".sec3ColLeft").addClass("fadeInLeftBig");
-        $(".sec3ColRight").addClass("fadeInRightBig");
-      }
-      if(i!==0) {
-        $(".sec0").removeClass("fadeIn")
-      }
-      if(i!==1) {
-        $(".sec1ColLeft").removeClass("fadeInLeftBig");
-        $(".sec1ColRight").removeClass("fadeInRightBig");
-      }
-      if(i!==2) {
-        $(".box").removeClass("zoomIn");
-        $(".img").removeClass("fadeInDownBig");
-        $(".is-3").removeClass("zoomIn");
-        $(".is-5").removeClass("fadeInUpBig");
-      }
-      if(i!==3) {
-        $(".sec3ColLeft").removeClass("fadeInLeftBig");
-        $(".sec3ColRight").removeClass("fadeInRightBig");
-      }
-    }
-  });
+    });
+  }
 });
